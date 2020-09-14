927c8afce88a5230c20fbf9bc356013d2ccba3f5
PRARTHANA HR <62104257+prarthanahr@users.noreply.github.com>
2020-09-14 16:50:06 +0530

    Update check.py

--- check.py
+++ check.py
@@ -6,12 +6,6 @@ def request_api_data(query_char):
     return res
 
 
-def get_password_leaks_count(hashes, hash_to_check):
-    hashes = (line.split(':') for line in hashes.text.splitlines())
-    for h, count in hashes:
-        if h == hash_to_check:
-            return count
-    return 0
 
 
 def pwned_api_check(password):
